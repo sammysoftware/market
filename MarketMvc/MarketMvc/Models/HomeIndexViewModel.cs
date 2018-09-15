b8d61aae838d1577b0ce8cefa8882bee9df21252
rching <rching@sammysoftware.com>
2018-09-15 15:51:10 -0700

    Added HomeIndexViewModel.cs

---
+++ MarketMvc/MarketMvc/Models/HomeIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NorthwindEntitiesLib;
+
+namespace MarketMvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public int VisitorCount;
+        public IList<Category> Categories { get; set; }
+        public IList<Product> Products { get; set; }
+    }
+}
