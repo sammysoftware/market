510e248bc2b4f175dc80608a6246a3f47e6520e1
rching <rching@sammysoftware.com>
2018-09-25 20:31:45 -0700

    Added navigation for Admin section

---
+++ MarketMvc/MarketMvc/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;   //[Authorize]
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;  //IMemoryCache
+using MarketMvc.DAL;
+using NorthwindEntitiesLib;
+
+namespace MarketMvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        readonly ILogger<HomeController> _logger;
+        private NorthwindDAL _NorthwindDAL;
+
+        public AdminController(NorthwindDbContext northwindCtx, IMemoryCache memoryCache, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _NorthwindDAL = new NorthwindDAL(northwindCtx, memoryCache, _logger);
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
